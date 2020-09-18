f1cd95e62731351031c794d683c7b5013d1b6e9c
nicolamassarenti <nicola.massarenti@gmail.com>
2020-09-18 15:20:26 +0200

    Added firestoreService

---
+++ src/services/firestoreService.py
@@ -0,0 +1,46 @@
+import firebase_admin
+import logging
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+logger = logging.getLogger(__name__)
+
+
+class FirestoreService:
+    def __init__(self):
+        """
+        Creates the service that connects to Firestore
+
+        :return None
+        """
+        self.__db = None
+
+        self._connect_to_db()
+
+    def _connect_to_db(self):
+        """
+        Creates the connection to Firestore
+
+        :return: None
+        """
+        firebase_admin.initialize_app()
+
+        self.__db = firestore.client()
+
+    def get_all_data(self, collection):
+        """
+        Retrieves all the data from a specific `collection`
+        :param collection: (string) the name of the collection
+        :return: array of documents
+        """
+
+        data = []
+
+        # Querying for the documents
+        collection_reference = self.__db.collection(collection)
+
+        for doc in collection_reference.stream():
+            logging.debug(u"Retrieved doc with id: `{}`".format(doc.id))
+            data.append(doc.to_dict())
+
+        return data
