7983c5628c83284a5c9a3312ed2218912514f73f
nicolamassarenti <nicola.massarenti@gmail.com>
2020-10-01 17:03:33 +0200

    Setup and script for training in the cloud

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import find_packages
 from setuptools import setup
+import os
 
-REQUIRED_PACKAGES = []
+
+# Read requirements
+requirements_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
+with open(requirements_file, "r") as f:
+    requirements = f.read().splitlines()
+
 
 setup(
     name='bert-faqclass-setup',
     version='0.1',
-    install_requires=REQUIRED_PACKAGES,
+    description='Bert-faqclass application package',
+    install_requires=requirements,
     packages=find_packages(),
     include_package_data=True,
-    description='Bert-faqclass application package'
 )
--- train_cloud.sh
+++ train_cloud.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
 # -e: Exit immediately if a command exits with a non-zero status, -v: Print shell input lines as they are read.
-set -ev
-
-JOB_NAME='JOB_NAME'
-JOB_DIR='JOB_OUTPUT_PATH'
+set -evx
 
 # VARIABLES
 MODEL_NAME="model_bert_faqclass"
-PACKAGE_PATH="$(pwd)/trainer"
-MODULE_NAME="$PACKAGE_PATH"/task
+PACKAGE_PATH="trainer"
+MODULE_NAME="$PACKAGE_PATH".task
 STAGING_BUCKET=$MODEL_NAME
 BUCKET_PATH="gs://$STAGING_BUCKET/"
 REGION="europe-west1"
+DATE=$(date '+%Y%m%d_%H%M%S')
+JOB_NAME="$STAGING_BUCKET"_"$DATE"_training
 JOB_DIR="$BUCKET_PATH$JOB_NAME"
 
 # Creating the bucket in the region if it doesn't already exists
 gsutil ls -b $BUCKET_PATH || gsutil mb -l $REGION $BUCKET_PATH
 
 # Setting job name
-DATE=$(date '+%Y%m%d_%H%M%S')
-JOB_NAME="$STAGING_BUCKET"_"$DATE"_training
 
 # Running the training on cloud
 gcloud ai-platform jobs submit training "$JOB_NAME" \
@@ -30,5 +27,4 @@ gcloud ai-platform jobs submit training "$JOB_NAME" \
   --region "$REGION" \
   --python-version 3.7 \
   --runtime-version 1.15 \
-  --job-dir "$JOB_DIR" \
-  --stream-logs
+  --job-dir "$JOB_DIR"
