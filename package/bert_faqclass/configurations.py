c276a87684ac8b4c47af5872e91624ec77d2a34b
nicolamassarenti <nicola.massarenti@gmail.com>
2020-11-20 16:29:54 +0100

    Creating configurations as object from dict

--- package/bert_faqclass/configurations.py
+++ package/bert_faqclass/configurations.py
@@ -1,3 +1,17 @@
 from bert_faqclass import _get_config_dict
+import json
 
-config = _get_config_dict(name="default")
+
+class ObjectFromDict:
+    # constructor
+    def __init__(self, dict1: dict):
+        self.__dict__.update(dict1)
+
+
+def dict2obj(dictionary: dict):
+    # using json.loads method and passing json.dumps
+    # method and custom object hook as arguments
+    return json.loads(json.dumps(dictionary), object_hook=ObjectFromDict)
+
+
+config = dict2obj(_get_config_dict(name="default"))
